f8d6a8ae5c5cf22ef00f44b098ea8041727164b7
jasengsg <jason.ng@insead.edu>
2015-02-18 17:46:40 +0100

    Fixed RunStudy.R

--- RunStudy.R
+++ RunStudy.R
@@ -17,7 +17,7 @@ local_directory <- paste(getwd())
 cat("\n *********\n WORKING DIRECTORY IS ", local_directory, "\n PLEASE CHANGE IT IF IT IS NOT CORRECT using setwd(...) - type help(setwd) for more information \n *********")
 
 # Please ENTER the name of the file with the data used. The file should contain a matrix with one row per observation (e.g. person) and one column per attribute. THE NAME OF THIS MATRIX NEEDS TO BE ProjectData (otherwise you will need to replace the name of the ProjectData variable below with whatever your variable name is, which you can see in your Workspace window after you load your file)
-datafile_name="OlympicsAthletesData" # do not add .csv at the end! make sure the data are numeric!!!! check your file!
+datafile_name="OlympicAthletesData" # do not add .csv at the end! make sure the data are numeric!!!! check your file!
 
 # Please ENTER the filename of the Report and Slides (in the doc directory) to generate 
 
@@ -41,8 +41,7 @@ start_local_webapp <- 0
 # Now run everything
 
 # this loads the selected data: DO NOT EDIT THIS LINE
-ProjectData <- read.csv(paste(local_directory, "/data/OlympicAthletesData.csv", sep = "/"), sep=",", dec=",") 
-
+ProjectData <- read.csv(paste(paste(local_directory,"data",sep="/"), paste(datafile_name,"csv",sep="."),sep="/"), sep=",", dec=",") 
 ProjectData=data.matrix(ProjectData) 
 
 colnames(ProjectData)<-gsub("\\.","  ",colnames(ProjectData))
